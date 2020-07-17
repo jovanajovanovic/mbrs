f75d337c28df626bbd8c8b2db6047da30f4c61b4
jovan996 <grgurjovan.jg@gmail.com>
2020-07-18 01:05:52 +0200

    added angular add and edit generator

--- generator/Generator/src/myplugin/GenerateAction.java
+++ generator/Generator/src/myplugin/GenerateAction.java
@@ -18,6 +18,7 @@ import com.thoughtworks.xstream.io.xml.DomDriver;
 
 import myplugin.analyzer.AnalyzeException;
 import myplugin.analyzer.ModelAnalyzer;
+import myplugin.generator.AngularAddEditEntityGenerator;
 import myplugin.generator.ControllerGenerator;
 import myplugin.generator.EJBGenerator;
 import myplugin.generator.RepositoryGenerator;
@@ -51,6 +52,7 @@ class GenerateAction extends MDAction{
 			generateService(analyzer, root, generatorOptions);
 			generateServiceImpl(analyzer, root, generatorOptions);
 			generateRepository(analyzer, root, generatorOptions);
+			generateAddEdit(analyzer, root, generatorOptions);
 		} catch (AnalyzeException e) {
 			JOptionPane.showMessageDialog(null, e.getMessage());
 		}
@@ -106,6 +108,19 @@ class GenerateAction extends MDAction{
 		generatorOptions = ProjectOptions.getProjectOptions().getGeneratorOptions().get("RepositoryGenerator");
 		RepositoryGenerator repositoryGenerator = new RepositoryGenerator(generatorOptions);
 		repositoryGenerator.generate();
+		JOptionPane.showMessageDialog(null, "Code is successfully generated! Generated code is in folder: " + generatorOptions.getOutputPath() +
+                ", package: " + generatorOptions.getFilePackage());
+		exportToXml();
+	}
+	
+	//************************ ANGULAR GENERATOR **************************
+	
+	private void generateAddEdit(ModelAnalyzer analyzer, Package root, GeneratorOptions generatorOptions) throws AnalyzeException {
+		analyzer = new ModelAnalyzer(root, "src/app");
+		analyzer.prepareModel();
+		generatorOptions = ProjectOptions.getProjectOptions().getGeneratorOptions().get("AngularAddEditEntityGenerator");
+		AngularAddEditEntityGenerator angularAddEditEntityGenerator = new AngularAddEditEntityGenerator(generatorOptions);
+		angularAddEditEntityGenerator.generate();
 		JOptionPane.showMessageDialog(null, "Code is successfully generated! Generated code is in folder: " + generatorOptions.getOutputPath() +
                 ", package: " + generatorOptions.getFilePackage());
 		exportToXml();
--- generator/Generator/src/myplugin/MyPlugin.java
+++ generator/Generator/src/myplugin/MyPlugin.java
@@ -33,6 +33,7 @@ public class MyPlugin extends com.nomagic.magicdraw.plugins.Plugin {
 		serviceOptions();
 		serviceImplOptions();
 		repositoryOptions();
+		angularAddEditOptions();
 	}
 	
 	private void ejbOptions() {
@@ -62,6 +63,14 @@ public class MyPlugin extends com.nomagic.magicdraw.plugins.Plugin {
 	private void repositoryOptions() {
 		GeneratorOptions generatorOptions = new GeneratorOptions("c:/temp", "repositoryclass", "templates", "{0}Repository.java", true, "repository"); 				
 		ProjectOptions.getProjectOptions().getGeneratorOptions().put("RepositoryGenerator", generatorOptions);
+		generatorOptions.setTemplateDir(pluginDir + File.separator + generatorOptions.getTemplateDir());
+	}
+	
+	//********* ANGULAR **************
+	
+	private void angularAddEditOptions() {
+		GeneratorOptions generatorOptions = new GeneratorOptions("c:/temp", "addEditEntity", "templates", "{0}AddEdit.java", true, "AddEdit"); 				
+		ProjectOptions.getProjectOptions().getGeneratorOptions().put("AngularAddEditEntityGenerator", generatorOptions);
 		generatorOptions.setTemplateDir(pluginDir + File.separator + generatorOptions.getTemplateDir());
 	}
 
